2b58c04c3caab923bf531c53cb5b9fda4d68c7b5
saurabhparmar08\Owner <vishal.joshi.testing@gmail.com>
2021-02-17 00:07:39 +0530

    Sample 1 commit

---
+++ src/main/java/Sample1/Sample2/App.java
@@ -0,0 +1,13 @@
+package Sample1.Sample2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
